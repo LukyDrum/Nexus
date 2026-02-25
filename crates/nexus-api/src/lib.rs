3a43a0491bab92cc79587a0feb91a590f63724b6
Luky Drum <lukbubi12@gmail.com>
2026-02-25 20:00:39 +0100

    Define basic messages

--- crates/nexus-api/src/lib.rs
+++ crates/nexus-api/src/lib.rs
@@ -1,7 +1,9 @@
+mod message;
 mod runtime_dir;
 mod socket;
 
 pub(crate) use runtime_dir::runtime_dir;
 
+pub use message::NexusMessage;
 pub use runtime_dir::NEXUS_COMMUNICATION_SOCKET;
 pub use socket::{NexusListener, NexusStream};
---
+++ crates/nexus-api/src/message.rs
@@ -0,0 +1,23 @@
+use serde::{Deserialize, Serialize};
+
+/// The variants of this enum represent the possible messages that will be passed between
+/// the parts of the shell using the [`NexusSocket`](crate::NexusSocket).
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum NexusMessage {
+    /// Switches to a workspace defined by a number in the **current group**.
+    /// It does not matter if the workspace already exists or not:
+    ///     - If it does, then it will just switch to it,
+    ///     - If it does not, then it will create it and switch to it.
+    ///
+    /// This mimics the behaviour of Hyprland.
+    SwitchWorkspace(u8),
+
+    /// Switches to a group defined by a string (the name of the group).
+    /// Same as with the workspace, it does not matter if the group already exists or not:
+    ///     - If it does, then it will switch to the last used workspace in that group,
+    ///     - If it does not, then it will create a new group and switch to workspace #1 in that group.
+    SwitchGroup(String),
+
+    /// Switches to the special hub workspace which exists outside of the groups.
+    SwitchToHub,
+}
