054b234ed0597e3a9d419246698cd445f9c3bac7
Luky Drum <lukbubi12@gmail.com>
2026-09-03 23:18:16 +0200

    Kool: Implement mpris on change function

--- crates/kool/src/language/libraries/mpris.rs
+++ crates/kool/src/language/libraries/mpris.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use mpris::{PlaybackStatus, Player, PlayerFinder};
 
 use crate::language::{Function, FunctionCode, FunctionParams, Library, SharedEnvironment, Value};
@@ -13,6 +15,7 @@ pub(super) fn mpris_library() -> Library {
         ("playPause", play_pause_function()),
         ("next", next_function()),
         ("previous", previous_function()),
+        ("onChange", on_change_function()),
     ])
 }
 
@@ -48,47 +51,8 @@ fn track_function() -> Function {
         let Some(player) = active_player() else {
             return Value::Null;
         };
-        let Ok(track) = player.get_metadata() else {
-            return Value::Null;
-        };
 
-        let title = track.title().map(Value::new_string).unwrap_or_default();
-        let album = track
-            .album_name()
-            .map(Value::new_string)
-            .unwrap_or_default();
-        let artist = track
-            .artists()
-            .map(|artists| Value::new_string(artists.join(", ")))
-            .unwrap_or_default();
-        let length = track
-            .length()
-            .map(|length| Value::Int(length.as_secs() as i64))
-            .unwrap_or_default();
-        let timestamp = player
-            .get_position_in_microseconds()
-            .map(|micros| Value::Int(micros as i64 * MICROS_IN_SECOND))
-            .unwrap_or_default();
-        let art_url = track.art_url().map(Value::new_string).unwrap_or_default();
-        let is_playing = player
-            .get_playback_status()
-            .map(|status| Value::Bool(matches!(status, PlaybackStatus::Playing)))
-            .unwrap_or_default();
-
-        let map = [
-            ("title", title),
-            ("album", album),
-            ("artist", artist),
-            ("length", length),
-            ("timestamp", timestamp),
-            ("artUrl", art_url),
-            ("isPlaying", is_playing),
-        ]
-        .into_iter()
-        .map(|(key, value)| (Value::new_string(key), value))
-        .collect();
-
-        Value::new_hash_map(map)
+        collect_track_info(&player).unwrap_or_default()
     };
 
     Function {
@@ -188,7 +152,96 @@ fn previous_function() -> Function {
     }
 }
 
+fn on_change_function() -> Function {
+    const TAIL_PARAM: &str = "tail";
+
+    let params = FunctionParams::default().with_tail(TAIL_PARAM);
+    let on_change_impl = |environment: &SharedEnvironment| -> Value {
+        let Some(Value::Function(callback)) = environment.get_variable(TAIL_PARAM) else {
+            return Value::Null;
+        };
+
+        std::thread::spawn(move || {
+            loop {
+                let Some(player) = active_player() else {
+                    // Lets wait a little and try again
+                    std::thread::sleep(Duration::from_secs(1));
+                    continue;
+                };
+                let Ok(mut events) = player.events() else {
+                    return;
+                };
+
+                let call_callback = || {
+                    let track_info = collect_track_info(&player).unwrap_or_default();
+
+                    let mut args = callback.default_args();
+                    let _ = args.set_singular_arg(track_info);
+
+                    let _ = callback.call(args);
+                };
+
+                // Init before the first event
+                call_callback();
+                while let Some(Ok(_event)) = events.next() {
+                    call_callback();
+                }
+            }
+        });
+
+        Value::Null
+    };
+
+    Function {
+        params,
+        code: FunctionCode::new_host(on_change_impl),
+        closure: None,
+    }
+}
+
 fn active_player() -> Option<Player> {
     let finder = PlayerFinder::new().ok()?;
     finder.find_active().ok()
+}
+
+fn collect_track_info(player: &Player) -> Option<Value> {
+    let track = player.get_metadata().ok()?;
+
+    let title = track.title().map(Value::new_string).unwrap_or_default();
+    let album = track
+        .album_name()
+        .map(Value::new_string)
+        .unwrap_or_default();
+    let artist = track
+        .artists()
+        .map(|artists| Value::new_string(artists.join(", ")))
+        .unwrap_or_default();
+    let length = track
+        .length()
+        .map(|length| Value::Int(length.as_secs() as i64))
+        .unwrap_or_default();
+    let timestamp = player
+        .get_position_in_microseconds()
+        .map(|micros| Value::Int(micros as i64 * MICROS_IN_SECOND))
+        .unwrap_or_default();
+    let art_url = track.art_url().map(Value::new_string).unwrap_or_default();
+    let is_playing = player
+        .get_playback_status()
+        .map(|status| Value::Bool(matches!(status, PlaybackStatus::Playing)))
+        .unwrap_or_default();
+
+    let map = [
+        ("title", title),
+        ("album", album),
+        ("artist", artist),
+        ("length", length),
+        ("timestamp", timestamp),
+        ("artUrl", art_url),
+        ("isPlaying", is_playing),
+    ]
+    .into_iter()
+    .map(|(key, value)| (Value::new_string(key), value))
+    .collect();
+
+    Some(Value::new_hash_map(map))
 }
