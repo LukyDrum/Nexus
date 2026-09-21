8e10975e2a6e227c5d2070817eb62f895287cbaa
LukyDrum <lukbubi12@gmail.com>
2026-09-21 16:49:41 +0200

    Hyprs: Add ctl functions

--- crates/hyprs/src/lib.rs
+++ crates/hyprs/src/lib.rs
@@ -1,6 +1,8 @@
+mod control;
 mod event;
 mod events;
 pub mod types;
 
+pub use control::{ControlError, hyprctl, hyprctl_eval};
 pub use event::{EventParseError, HyprlandEvent};
 pub use events::{HyprlandEvents, HyprlandEventsError};
